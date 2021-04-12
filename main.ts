269685176563562ba422f705a342f0540f827088
abdelkader MOUDJAR <abdoumjr@gmail.com>
2021-04-12 14:07:36 +0100

    get books

--- main.ts
+++ main.ts
@@ -1,9 +1,42 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router, RouterContext } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
+const PORT = 3333
+const router = new Router()
+
+type book  =  {
+    Id  : String,
+    Name : String,
+    Author :  String,
+    BookType : String,
+    Price : Number
+}
+
+const  books  : Array<book> =  [
+    {
+                    Id  : "id1",
+					Name :"Harry Potter and the Philosopher's Stone",
+					Author : "J. K. Rowling",
+					BookType : "fantasy",
+					Price  : 19,
+    }
+]
+
+router.get('/api',(context : RouterContext)=>{
+    context.response.body =   'Silence is golden'
+})
+
+router.get('/api/books',(context : RouterContext)=>{
+    context.response.body =   books
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 app.use((ctx) => {
 ctx.response.body = "Hello World!";
 });
 
-await app.listen({ port: 3333 });
+console.log('Server is up')
+
+await app.listen({ port: PORT });
